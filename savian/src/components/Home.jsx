84444bfab821a1d6dc9b73572c1b4e04fd679191
Sravya <sravya6051@gmail.com>
2023-06-20 15:57:16 +0530

    basic home page

--- savian/src/components/Home.jsx
+++ savian/src/components/Home.jsx
@@ -1,11 +1,24 @@
 import React from "react";
-import '../components/home.css'
+import '../components/home.css';
+
+
 
 export const Home=()=>{
     return(
         <div className="home_container">
-           <p>Explore builders</p>
-           <input placeholder="Search here"></input> 
+            <div className="home_container_img">
+                <p>Explore builders</p>
+                <input placeholder="Search here"></input> 
+            </div>
+            <div className="home_bottom">
+                
+                <p className="home_bottom_heading">Explore the Builders</p>
+                <div className="builder_container">
+                    builder
+                </div>
+        
+            </div>
+           
         </div>
     );
 }
