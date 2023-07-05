5a5a8d04aabaf26e562a56a4eb44d0fbfa498252
Sravya <sravya6051@gmail.com>
2023-07-05 13:28:18 +0530

    added aos to what is,channged underline

--- savian/src/components/whatisconsulting/Whatis.jsx
+++ savian/src/components/whatisconsulting/Whatis.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import './whatis.css'
+import './whatis.css';
+import dots from "../../images/dots1.jpg";
 
 const Whatis=()=>{
     return(
-        <div className="whatis_container">
-            <p className="whatis_heading">What is Real Estate Consulting</p>
-            <p className="whatis_content">Real estate investment consulting is a specialized service that provides professional advice and guidance to individuals and businesses interested in making real estate investments. It involves working with experienced consultants who have in-depth knowledge of the real estate market and can help clients make informed investment decisions</p>
-           
+        <div className="whatis_container">            
+        
+            <p data-aos="fade-up" data-aos-duration="1000" className="whatis_heading">What is Real Estate Investment Consulting</p>
+            <p data-aos="fade-up" data-aos-duration="1000" className="whatis_content">Real estate investment consulting is a specialized service that provides professional advice and guidance to individuals and businesses interested in making real estate investments. It involves working with experienced consultants who have in-depth knowledge of the real estate market and can help clients make informed investment decisions</p>
+        
         </div>
     );
 }
